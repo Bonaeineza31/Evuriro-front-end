e6fd9bdaa60495c90e5058db406303ad43b520ca
Bonaeineza31 <b.ineza@alustudent.com>
2025-02-28 09:32:09 +0200

    linked components

--- frontend/components/Layout.jsx
+++ frontend/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar';
-import Sidebar from './Sidebar';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
 
 import '../styles/layout.css';
 
