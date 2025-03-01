3c0cadaed1bd23dd4a50860bc08e1939928e2187
Bonaeineza31 <b.ineza@alustudent.com>
2025-03-02 00:50:54 +0200

    added the image

--- frontend/pages/Teleconsultation.jsx
+++ frontend/pages/Teleconsultation.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import '../styles/Teleconsultation.css';
-import sarah
+import sarah from "../images/Screenshot 2025-03-01 223549.png"
 
 const HeartIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#2563eb" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
