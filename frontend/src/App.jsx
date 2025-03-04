2db42b9498b66b333e30590daadd6a2592a6f66f
Bonaeineza31 <b.ineza@alustudent.com>
2025-03-04 16:24:59 +0200

    wrote it well

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar'; 
 import './App.css';
 import { ThemeProvider } from '../pages/Theme';
-import { LanguageProvider } from './LanguageSelector';
+import { LanguageProvider } from '../src/Languages';
 
 
 // Import pages
